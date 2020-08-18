7ad3f389a4a203c6378d6b35cda89ee5fe6e1654
govardhan8297 <66250220+govardhan8297@users.noreply.github.com>
2020-08-18 21:59:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function displayFunction() {
+    var name = prompt("Enter Your Name To Join:");
+    alert("Hello " + name + ", Welcome To Our Kallam Club");
+    document.getElementById("name").innerHTML = name + " Has Been Joined.(+1)";
+}
+
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("intro-para");
+    x.innerHTML = "Gulabiii";
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        alert("GeoLocation is not supported on your Browser");
+    }
+}
+function showPosition(position) {
+    alert("Your Latitude: " + position.coords.latitude + " and Longitude: " + position.coords.longitude);
+}
+
+
+
+function editFunction() {
+    document.designMode = "on";
+}
+
+
