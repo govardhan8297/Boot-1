8164691f8f60831677960be7b23b7462839869d9
govardhan8297 <66250220+govardhan8297@users.noreply.github.com>
2020-08-20 16:49:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function displayName() {
     var date = new Date();
+    var am_pm = date.getHours() >= 12 ? "PM" : "AM";
     var name = prompt("Enter Your Name To Join:");
     alert("Hello " + name + ", Welcome To Our Kallam Club");
-    document.getElementById("name").innerHTML = name + " Has Been Joined on " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " at " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    document.getElementById("name").innerHTML = name + " Has Been Joined on " + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " at " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + am_pm;
 
 }
 
